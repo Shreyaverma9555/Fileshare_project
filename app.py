0ca8ed1c377dd90d7172016efaeb6e9ebd8680d7
shreya <197284366+Shreyaverma9555@users.noreply.github.com>
2026-04-02 22:22:25 +0530

    final mail config

--- app.py
+++ app.py
@@ -61,14 +61,16 @@ init_db()
 
 # ------------------ MAIL CONFIG ------------------
 app.config['MAIL_SERVER'] = 'sandbox.smtp.mailtrap.io'
-app.config['MAIL_PORT'] = 587
+app.config['MAIL_PORT'] = 2525
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
 app.config['MAIL_USERNAME'] = os.environ.get("MAIL_USERNAME")
 app.config['MAIL_PASSWORD'] = os.environ.get("MAIL_PASSWORD")
+app.config['MAIL_DEFAULT_SENDER'] = os.environ.get("MAIL_USERNAME")
 app.config['MAX_CONTENT_LENGTH'] = 50 * 1024 * 1024
 
 app.config['MAIL_MAX_EMAILS'] = None
+app.config['MAIL_SUPPRESS_SEND'] = False
 
 mail = Mail(app)
 
