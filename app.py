f144c7ed005869c537ae959d6dbc1f31fa656ad5
shreya <197284366+Shreyaverma9555@users.noreply.github.com>
2026-04-02 22:11:07 +0530

    fix mail config

--- app.py
+++ app.py
@@ -61,12 +61,14 @@ init_db()
 
 # ------------------ MAIL CONFIG ------------------
 app.config['MAIL_SERVER'] = 'sandbox.smtp.mailtrap.io'
-app.config['MAIL_PORT'] = 2525
+app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
 app.config['MAIL_USERNAME'] = os.environ.get("MAIL_USERNAME")
 app.config['MAIL_PASSWORD'] = os.environ.get("MAIL_PASSWORD")
 app.config['MAX_CONTENT_LENGTH'] = 50 * 1024 * 1024
+
+app.config['MAIL_MAX_EMAILS'] = None
 
 mail = Mail(app)
 
@@ -77,6 +79,7 @@ def send_otp_email(receiver_email, otp):
     try:
         msg = Message(
             subject="OTP Verification",
+            sender=app.config['MAIL_USERNAME'],
             recipients=[receiver_email],
             body=f"Your OTP is: {otp}"
         )
