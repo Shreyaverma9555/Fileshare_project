6ea38841784ff994095cc25779d4f5b27c085028
shreya <197284366+Shreyaverma9555@users.noreply.github.com>
2026-04-03 10:23:12 +0530

    final mailtrap fix

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ init_db()
 
 # ------------------ MAIL CONFIG ------------------
 app.config['MAIL_SERVER'] = 'sandbox.smtp.mailtrap.io'
-app.config['MAIL_PORT'] = 2525
+app.config['MAIL_PORT'] = 587 
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
 app.config['MAIL_USERNAME'] = os.environ.get("MAIL_USERNAME")
